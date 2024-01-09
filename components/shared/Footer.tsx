fcafc7eec65b83a1cfe8f9578a0fd0341682a5b9
ahmadnusa <ahmadnursaefullah79@gmail.com>
2024-01-09 15:58:52 +0700

    setup Footer and Home page

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className=" wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128.28}
+            height={38}
+            priority
+          />
+        </Link>
+        <p>2023 Evently, All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
 }
