8bc1097d3e101fbf7af3ee6bb525a840045473f3
ahmadnusa <ahmadnursaefullah79@gmail.com>
2024-01-10 13:25:36 +0700

    fix some typo

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const mongodbUri = process.env.MONGODB_URI;
-let cached = (global as any).mongoose || { conn: null, promise: null };
+const mongodbUri = process.env.MONGODB_URI
+let cached = (global as any).mongoose || { conn: null, promise: null }
 
 export async function connectToDatabase() {
-  if (cached.conn) return cached.conn;
-  if (!mongodbUri) return "Missing MONGODB_URI";
+  if (cached.conn) return cached.conn
+  if (!mongodbUri) return 'Missing MONGODB_URI'
   cached.promise =
     cached.promise ||
     mongoose.connect(mongodbUri, {
-      dbName: "evently",
+      dbName: 'evently',
       bufferCommands: false,
-    });
+    })
 
-  cached.conn = await cached.promise;
-  return cached.conn;
+  cached.conn = await cached.promise
+  return cached.conn
 }
