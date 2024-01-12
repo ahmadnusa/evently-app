a58ae456ac4242774562a5aa349eb56caec31dc8
ahmadnusa <ahmadnursaefullah79@gmail.com>
2024-01-12 20:04:14 +0700

    add order detail page

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,14 +1,22 @@
 import Collection from '@/components/shared/Collection'
 import { Button } from '@/components/ui/button'
 import { getEventsByUser } from '@/lib/actions/event.actions'
+import { getOrdersByUser } from '@/lib/actions/order.actions'
+import { IOrder } from '@/lib/database/models/order.model'
+import { SearchParamProps } from '@/types'
 import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
 
-export default async function ProfilePage() {
+export default async function ProfilePage({ searchParams }: SearchParamProps) {
   const { sessionClaims } = auth()
   const userId = sessionClaims?.userId as string
+  const ordersPage = Number(searchParams?.ordersPage) || 1
+  const eventsPage = Number(searchParams?.eventsPage) || 1
 
-  const organizedEvents = await getEventsByUser({ userId, page: 1 })
+  const orders = await getOrdersByUser({ userId, page: ordersPage })
+
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event || [])
+  const organizedEvents = await getEventsByUser({ userId, page: eventsPage })
 
   return (
     <>
@@ -22,18 +30,18 @@ export default async function ProfilePage() {
         </div>
       </section>
 
-      {/* <section className="wrapper my-8">
+      <section className="wrapper my-8">
         <Collection
-          data={events?.data}
+          data={orderedEvents}
           emptyTitle="No Event tickets purchased yet"
           emptyStateSubText="No worries - plenty of exciting events to explore! "
           collectionType="My_Tickets"
           limit={3}
-          page={1}
+          page={ordersPage}
           urlParamName="ordersPage"
-          totalPages={2}
+          totalPages={orders?.totalPages}
         />
-      </section> */}
+      </section>
 
       {/* {events Organized} */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
@@ -52,9 +60,9 @@ export default async function ProfilePage() {
           emptyStateSubText="Go create your first event! "
           collectionType="Events_Organized"
           limit={3}
-          page={1}
+          page={eventsPage}
           urlParamName="eventsPage"
-          totalPages={2}
+          totalPages={organizedEvents?.totalPages}
         />
       </section>
     </>
