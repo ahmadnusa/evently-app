5de44116d8a5ada89bab05e492926f7ac2232da9
ahmadnusa <ahmadnursaefullah79@gmail.com>
2024-01-12 19:08:42 +0700

    fix user model

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -64,7 +64,7 @@ export async function POST(req: Request) {
       email: email_addresses[0].email_address,
       username: username!,
       firstName: first_name,
-      lastName: last_name,
+      lastName: last_name || '',
       photo: image_url,
     }
 
--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { IEvent } from '@/lib/database/models/event.model'
-import { SignedIn, SignedOut, useUser } from '@clerk/nextjs'
+import { SignedIn, SignedOut, auth, useUser } from '@clerk/nextjs'
 import { Button } from '../ui/button'
 import Link from 'next/link'
 import Checkout from './Checkout'
@@ -9,6 +9,7 @@ import Checkout from './Checkout'
 export default function CheckoutButton({ event }: { event: IEvent }) {
   const { user } = useUser()
   const userId = user?.publicMetadata.userId as string
+
   const hasEventFinished = new Date(event.endDateTime) < new Date()
   return (
     <div className="flex items-center gap-3">
